f30d2092e26b560fe53caba35ad364ca2a5c302a
timholy <tim.holy@gmail.com>
2013-06-17 05:55:15 -0500

    Fix #18

--- src/Profile.jl
+++ src/Profile.jl
@@ -1,2 +1,10 @@
 include("sprofile.jl")
 include("iprofile.jl")
+
+module Profile
+using SProfile
+using IProfile
+
+export @sprofile, sprofile_clear, sprofile_flat, sprofile_init, sprofile_tree, @iprofile
+
+end
